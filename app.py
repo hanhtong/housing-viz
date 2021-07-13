19e91256588864195d73d3ba529376affeb629b2
Hanh Tong <hanh.tong@tap.work>
2021-07-12 17:41:39 -0700

    Update app.py and requirements.txt

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-from jupyter_dash import JupyterDash
 import pandas as pd
 
 import warnings
